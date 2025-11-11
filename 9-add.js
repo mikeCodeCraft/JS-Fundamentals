b4f999b56d2c875487c905f13c0746dd3ba6e83b
michael <michaelokon565@gmail.com>
2025-11-11 19:50:58 +0100

    initial commit

---
+++ 9-add.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+
+function add(a, b) {
+  return a + b;
+}
+
+const firstInt = parseInt(args[0]);
+const secondInt = parseInt(args[1]);
+
+if (isNaN(firstInt) || isNaN(secondInt)) {
+  console.log('Please provide two integers');
+} else {
+  console.log(add(firstInt, secondInt));
+}
